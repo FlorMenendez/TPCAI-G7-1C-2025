1dce2c0415f8f14e26763bc968655490dddce3a1
JulianDiaz <juli.diaz277@hotmail.com>
2025-05-18 12:21:46 -0300

    Persistencia

--- TemplateTPCorto/Persistencia/UsuarioPersistencia.cs
+++ TemplateTPCorto/Persistencia/UsuarioPersistencia.cs
@@ -5,18 +5,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Persistencia
 {
     public class UsuarioPersistencia
     {
-        public Credencial login(String username)
+        public Credencial login(string username)
         {
-            DataBaseUtils dataBaseUtils = new DataBaseUtils();
-            List<String> registros = dataBaseUtils.BuscarRegistro(username);
+            string path = @"..\..\..\Persistencia\DataBase\Tablas\credenciales.csv";
 
-            Credencial credencial = new Credencial(registros[0]);
-            return credencial;
+            if (!File.Exists(path))
+                return null;
+
+            var lineas = File.ReadAllLines(path);
+
+            for (int i = 1; i < lineas.Length; i++) // Saltamos encabezado
+            {
+                string linea = lineas[i];
+
+                if (string.IsNullOrWhiteSpace(linea))
+                    continue;
+
+                string[] campos = linea.Split(';');
+
+                if (campos.Length < 5)
+                    continue;
+
+                if (campos[1].Trim().Equals(username.Trim(), StringComparison.OrdinalIgnoreCase))
+                {
+                    return new Credencial(linea);
+                }
+            }
+
+            return null;
         }
     }
 }
