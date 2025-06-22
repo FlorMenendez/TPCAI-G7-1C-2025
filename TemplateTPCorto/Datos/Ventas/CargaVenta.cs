38c2bd7fda316ea827ac9333bb90fcb85182423e
mauroacortes <53631313+mauroacortes@users.noreply.github.com>
2025-06-22 20:24:04 -0300

    Agrego capa de datos

---
+++ TemplateTPCorto/Datos/Ventas/CargaVenta.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos.Ventas
+{
+    public class CargaVenta
+    {
+        public Guid IdCliente { get; set; }
+        public Guid IdUsuario { get; set; }
+        public Guid IdProducto { get; set; }
+        public int Cantidad { get; set; }
+    }
+}
