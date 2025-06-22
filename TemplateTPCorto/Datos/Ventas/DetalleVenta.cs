a656d033d5cbdee9ae4c54cd5769ba0ddb4a90c5
JulianDiaz <juli.diaz277@hotmail.com>
2025-06-22 20:30:13 -0300

    Se Actualizo datos y capa de negocios

---
+++ TemplateTPCorto/Datos/Ventas/DetalleVenta.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos.Ventas
+{
+    public class DetalleVenta
+    {
+        public Guid IdProducto { get; set; }
+        public int Cantidad { get; set; }
+        public int PrecioUnitario { get; set; }
+    }
+}
